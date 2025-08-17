118c21a360c38ed6707d930ab86add634dc2e4ea
Lawrence <lawrenceowino96@gmail.com>
2025-08-16 23:01:40 -0700

    Updated task_4.sq, task_5.sql, task_6.sql file

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Select the database
 USE alx_book_store;
 
--- Show full description of the table Books
-SHOW CREATE TABLE Books;
+-- Show full description of Books table using INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
